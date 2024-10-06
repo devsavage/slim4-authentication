ee6f23a3a288b97096cd4bda364847728744a88e
Savage <savage@savagedev.io>
2024-10-05 22:40:41 -0400

    Begin initial setup

---
+++ bootstrap/app.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Copyright (c) 2024 devsavage (https://github.com/devsavage)
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+use DI\Bridge\Slim\Bridge;
+use DI\ContainerBuilder;
+use Dotenv\Dotenv;
+use Noodlehaus\Config;
+
+session_start();
+
+require __DIR__ . "/../vendor/autoload.php";
+
+const INC_ROOT = __DIR__;
+
+if(file_exists($env = INC_ROOT . "/../.env")) {
+    $dotenv = Dotenv::createImmutable($env);
+    $dotenv->load();
+}
+
+$builder = new ContainerBuilder();
+
+$default = require INC_ROOT . "/../bootstrap/container.php";
+$default($builder);
+
+$container = $builder->build();
+
+$container->set("config", function() {
+    return new Config(INC_ROOT . "/../config");
+});
+
+$config = $container->get("config");
+
+$app = Bridge::create($container);
+$app->setBasePath($config->get("app.base_path"));
