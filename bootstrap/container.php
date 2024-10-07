134ae47f8597749370bb81e07cdfe96ede49e823
Savage <savage@savagedev.io>
2024-10-06 21:43:21 -0400

    Remove unused variable

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -71,10 +71,6 @@ return function (ContainerBuilder $builder)
                 "user" => $container->get("auth")->user(),
             ]);
 
-            $routeParser = $container->get(RouteParserInterface::class);
-
-//            $twig->getEnvironment()->addGlobal("route", $twig->get);
-
             return $twig;
         },
 
